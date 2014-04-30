9779af8fb1253671435505809e5f3bb287dbe629
AleksueiR <aleksuei.riabtsev@ec.gc.ca>
2014-04-30 10:41:10 -0400

    Update stylish.js
    option to not use colors

--- stylish.js
+++ stylish.js
@@ -6,59 +6,63 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = {
-	reporter: function (result, config, options) {
-		var total = result.length;
-		var ret = '';
-		var headers = [];
-		var prevfile;
+    reporter: function(result, config, options) {
+        var total = result.length;
+        var ret = '';
+        var headers = [];
+        var prevfile;
 
-		options = options || {};
-		
-		var colors = {};
-		if(fs.existsSync(path.resolve('.stylishcolors'))) {
-			var rc = fs.readFileSync('.stylishcolors', { encoding: 'utf8' });
-			colors = JSON.parse(rc);
-		}
-			
-		colors = extend({
-			'meta': 'gray',
-			'reason': 'blue',
-			'verbose': 'gray',
-			'error': 'red',
-			'noproblem': 'green'
-		}, colors);
+        options = options || {};
 
-		ret += table(result.map(function (el, i) {
-			var err = el.error;
-			var line = [
-				'',
-				chalk[colors.meta]('line ' + err.line),
-				chalk[colors.meta]('col ' + err.character),
-				chalk[colors.reason](err.reason)
-			];
+        var colors = {
+            'meta': 'gray',
+            'reason': 'blue',
+            'verbose': 'gray',
+            'error': 'red',
+            'noproblem': 'green'
+        };
 
-			if (el.file !== prevfile) {
-				headers[i] = el.file;
-			}
+        if (fs.existsSync(path.resolve('.stylishcolors'))) {
+            var rc = fs.readFileSync('.stylishcolors', {
+                encoding: 'utf8'
+            });
+            colors = JSON.parse(rc);
+        } else {
+            colors = null;
+        }
 
-			if (options.verbose) {
-				line.push(chalk[colors.verbose]('(' + err.code + ')'));
-			}
+        ret += table(result.map(function(el, i) {
+            var err = el.error;
+            var line = [
+                '',
+                colors ? chalk[colors.meta]('line ' + err.line) : 'line ' + err.line,
+                colors ? chalk[colors.meta]('col ' + err.character) : 'col ' + err.character,
+                colors ? chalk[colors.reason](err.reason) : err.reason
+            ];
 
-			prevfile = el.file;
+            if (el.file !== prevfile) {
+                headers[i] = el.file;
+            }
 
-			return line;
-		})).split('\n').map(function (el, i) {
-			return headers[i] ? '\n' + chalk.underline(headers[i]) + '\n' + el : el;
-		}).join('\n') + '\n\n';
+            if (options.verbose) {
+                line.push(
+                	colors ? chalk[colors.verbose]('(' + err.code + ')') : '(' + err.code + ')');
+            }
 
-		if (total > 0) {
-			ret += chalk[colors.error].bold('✖ ' + total + ' problem' + (total === 1 ? '' : 's'));
-		} else {
-			ret += chalk[colors.noproblem].bold('✔ No problems');
-			ret = '\n' + ret.trim();
-		}
+            prevfile = el.file;
 
-		console.log(ret + '\n');
-	}
+            return line;
+        })).split('\n').map(function(el, i) {
+            return headers[i] ? '\n' + colors ? chalk.underline(headers[i]) : headers[i] + '\n' + el : el;
+        }).join('\n') + '\n\n';
+
+        if (total > 0) {
+            ret += colors ? chalk[colors.error].bold('✖ ' + total + ' problem' + (total === 1 ? '' : 's')) : '✖ ' + total + ' problem' + (total === 1 ? '' : 's');
+        } else {
+            ret += colors ? chalk[colors.noproblem].bold('✔ No problems') : '✔ No problems';
+            ret = '\n' + ret.trim();
+        }
+
+        console.log(ret + '\n');
+    }
 };
